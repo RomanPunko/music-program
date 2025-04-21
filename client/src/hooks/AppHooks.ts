115ccc616f8763773061b1df0fd363ecae0833b0
Roman Punko <romanpunko2004@gmail.com>
2025-04-21 15:10:38 +0300

    + типізація

--- client/src/hooks/AppHooks.ts
+++ client/src/hooks/AppHooks.ts
@@ -4,5 +4,7 @@ import type { AppDispatch, AppStore, RootState } from '../store/store'
 
 
 export const useAppDispatch: () => AppDispatch = useDispatch
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
 export const useAppStore: () => AppStore = useStore
